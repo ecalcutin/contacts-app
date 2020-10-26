9223a1cb562366b7c1ebb783baedd9a3624a97ca
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-10-26 18:08:59 +0200

    Add env schema

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { GraphQLModule } from '@nestjs/graphql';
 
-
+import { ENV_SCHEMA } from './env.schema';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [],
+      validationSchema: ENV_SCHEMA,
+      expandVariables: true,
+      validationOptions: {
+        allowUnknow: false,
+        abortEarly: true
+      }
+    }),
     // GraphQLModule.forRoot({
     //   playground: true
     // })
