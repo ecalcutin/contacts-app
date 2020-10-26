88ff536e20c3899712a43ea33cf55027ad9b7939
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-10-26 22:39:31 +0200

    Fix styles

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { ContactsComponent } from './pages/contacts/contacts.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'popular', pathMatch: 'full' },
+  { path: '', redirectTo: 'contacts', pathMatch: 'full' },
   { path: 'popular', component: PopularComponent },
   { path: 'contacts', component: ContactsComponent },
-  { path: '**', redirectTo: 'popular' }
+  { path: '**', redirectTo: 'contacts' }
 ];
 
 @NgModule({
