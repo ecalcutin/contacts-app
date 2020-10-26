c87a739dcae97df657fabe2344e0c753d8a78734
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-10-26 16:23:00 +0200

    Add microservice package

--- microservices/users/src/main.ts
+++ microservices/users/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const microserviceOpts: MicroserviceOptions = {
+  transport: Transport.TCP,
+  options: {
+    port: 8080
+  }
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOpts);
+  await app.listen(() => { });
 }
 bootstrap();
