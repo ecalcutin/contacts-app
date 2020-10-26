d7e1cfe4027c5527dc2d08bb20b5d5a0cf534bc3
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-10-26 18:04:14 +0200

    Updated packages && installed graphql

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 
 @Module({
-  imports: [],
+  imports: [
+    // GraphQLModule.forRoot({
+    //   playground: true
+    // })
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
